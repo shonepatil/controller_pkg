a77a8372a6745039f8c72a2310e1dc03e4c00498
shonepatil <s3patil@ucsd.edu>
2022-05-13 14:18:09 -0700

    added cross track error

--- controller_pkg/pid_node.py
+++ controller_pkg/pid_node.py
@@ -1,7 +1,10 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from std_msgs.msg import Float32MultiArray, MultiArrayDimension, MultiArrayLayout
+from nav_msgs.msg import Path, Odometry
 from ackermann_msgs.msg import AckermannDriveStamped
+from tf_transformations import euler_from_quaternion, quaternion_from_euler
 import time
 import math
 import os
@@ -11,22 +14,32 @@ NODE_NAME = 'pid_node'
 ERROR_TOPIC_NAME = '/error'
 ACTUATOR_TOPIC_NAME = '/teleop'
 
+PATH_TOPIC_NAME = '/global_trajectory'
+ODOM_TOPIC_NAME = '/ego_racecar/odom'
 
 class PidController(Node):
     def __init__(self):
         super().__init__(NODE_NAME)
         self.QUEUE_SIZE = 10
 
+        self.path_thread = MutuallyExclusiveCallbackGroup()
+        self.odom_thread = MutuallyExclusiveCallbackGroup()
+
         # Actuator control
         self.drive_pub = self.create_publisher(AckermannDriveStamped, ACTUATOR_TOPIC_NAME, self.QUEUE_SIZE)
         self.drive_cmd = AckermannDriveStamped()
 
-        # Error subscriber
-        self.error_subscriber = self.create_subscription(Float32MultiArray, ERROR_TOPIC_NAME, self.error_measurement, self.QUEUE_SIZE)
-        self.error_subscriber
+        # Get Odometry measurements
+        self.odom_subscriber = self.create_subscription(Odometry, ODOM_TOPIC_NAME, self.odom_measurement, 10, callback_group=self.odom_thread)
+        self.odom_subscriber
 
-        # Speed subscriber
+        # # Error subscriber
+        # self.error_subscriber = self.create_subscription(Float32MultiArray, ERROR_TOPIC_NAME, self.error_measurement, self.QUEUE_SIZE)
+        # self.error_subscriber
 
+        # Get Reference Trajectory
+        self.path_subscriber = self.create_subscription(Path, PATH_TOPIC_NAME, self.set_path, rclpy.qos.qos_profile_sensor_data, callback_group=self.path_thread)
+        self.path_subscriber
 
         # setting up message structure for vesc-ackermann msg
         self.current_time = self.get_clock().now().to_msg()
@@ -65,6 +78,15 @@ class PidController(Node):
         self.max_left_steering=self.get_parameter('max_left_steering').value  # between abs([0,0.436332]) radians (0-25degrees)
 
         # initializing PID control
+        self.x = 0
+        self.y = 0
+        self.x_buffer = 0
+        self.y_buffer = 0
+
+        self.x_path = []
+        self.y_path = []
+        self.line_error_threshold = 0.001
+
         self.e_y_buffer = 0
         self.e_x_buffer = 0
         self.e_theta_buffer = 0
@@ -95,14 +117,90 @@ class PidController(Node):
         self.Ts = 0.01  # contoller sample time
         self.create_timer(self.Ts, self.controller)
 
-    def error_measurement(self, error_data):
-        error_data_check = np.array([error_data.data[0], error_data.data[1], error_data.data[2]])
-        if not (np.isnan(error_data_check).any()):
-            self.e_y_buffer = error_data.data[0]
-            self.e_x_buffer = error_data.data[1]
-            self.e_theta_buffer = error_data.data[2]
+    def odom_measurement(self, odom_data):
+        # TODO: what is frequency of data coming in?
+
+        # car position
+        self.x_buffer = odom_data.pose.pose.position.x
+        self.y_buffer = odom_data.pose.pose.position.y
+
+        # car orientation
+        # FIXME: confirm coordinate axes
+        quaternion = (odom_data.orientation.x, odom_data.orientation.y, odom_data.orientation.z, odom_data.orientation.w)
+        euler = euler_from_quaternion(quaternion)
+        self.yaw_buffer = euler[2]
+
+        # car velocity
+        self.vx_buffer = odom_data.twist.twist.linear.x
+        self.vy_buffer = odom_data.twist.twist.linear.y
+
+    # def error_measurement(self, error_data):
+    #     error_data_check = np.array([error_data.data[0], error_data.data[1], error_data.data[2]])
+    #     if not (np.isnan(error_data_check).any()):
+    #         self.e_y_buffer = error_data.data[0]
+    #         self.e_x_buffer = error_data.data[1]
+    #         self.e_theta_buffer = error_data.data[2]
+
+    def set_path(self, path_data):
+        # TODO: Currently not working with Lidar Nav
+
+        # path orientation 
+        # FIXME: confirm coordinate axes
+        # quaternion = (path_data.poses[0].pose.orientation.x, path_data.poses[0].pose.orientation.y,
+        #               path_data.poses[0].pose.orientation.z, path_data.poses[0].pose.orientation.w)
+        # euler = euler_from_quaternion(quaternion)
+        # self.yaw_path = euler[2]
+
+        # path coordinates (GLOBAL)
+        self.x_path = np.array([pose.pose.position.x for pose in path_data.poses])
+        self.y_path = np.array([pose.pose.position.x for pose in path_data.poses])
+        self.get_logger().info(f"first val PATH (x): ({self.x_path[0]})")
+
+    def get_cross_track_error(self):
+        # self.get_logger().info("Updating CROSS-TRACK-ERROR")
+        
+        # find 2 closest points in path with car
+        error_x = self.x_path - self.x
+        error_y = self.y_path - self.y
+        error_mag = np.power(np.power(error_x,2) + np.power(error_y, 2), 0.5)
+        error_mag1, error_mag2 = np.partition(error_mag, 1)[0:2]
+        error_mag1_index = np.argwhere(error_mag == error_mag1)[0][0]
+        error_mag2_index = np.argwhere(error_mag == error_mag2)[0][0]
+        Px1 = self.x_path[error_mag1_index]
+        Px2 = self.x_path[error_mag2_index]
+        Py1 = self.y_path[error_mag1_index]
+        Py2 = self.y_path[error_mag2_index]
+        
+        # create line extrapolations to determine cross-track error
+        # (threshold added to account for zero/infinite slopes)
+        
+        # path line
+        delta_x = Px2 - Px1 + self.line_error_threshold
+        delta_y = Py2 - Py1 + self.line_error_threshold
+        theta_path = float(np.arctan2(delta_y, delta_x))
+        path_slope = delta_y / delta_x
+        path_intercept = Py1 - path_slope * Px1
+        
+        # car line
+        car_slope = -1 / path_slope
+        car_intercept = self.y - car_slope * self.x
+        ecg_x = (path_intercept - car_intercept) / (car_slope - path_slope)
+        ecg_y = car_slope * ecg_x + car_intercept
+
+        # get actual cross-track error distance and use sign of slope to determine direction
+        ecg_r = np.power(np.power((self.x - ecg_x),2) + np.power((self.y - ecg_y), 2), 0.5)
+        e_cg_sign = np.sign(car_slope)
+        e_cg = float(e_cg_sign * ecg_r)
+        self.get_logger().info(f"{e_cg},{theta_path}")
+        return ecg_x, ecg_y, e_cg, theta_path
 
     def get_latest_measurements(self):
+        ecg_x, ecg_y, e_cg, theta_path = self.get_cross_track_error()
+
+        self.e_y_buffer = ecg_x
+        self.e_x_buffer = ecg_y
+        self.e_theta_buffer = theta_path
+
         self.e_y = self.e_y_buffer
         self.e_x = self.e_x_buffer
         self.e_theta = self.e_theta_buffer
@@ -139,29 +237,31 @@ class PidController(Node):
                                )
         self.e_y_1 = self.e_y
 
-        if self.e_x < self.long_upper_error_threshold:
-            # Publish values
-            try:
-                # publish drive control signal
-                self.drive_cmd.header.stamp = self.current_time
-                self.drive_cmd.header.frame_id = self.frame_id
-                self.drive_cmd.drive.speed = speed
-                self.drive_cmd.drive.steering_angle = -delta
-                self.drive_pub.publish(self.drive_cmd)
+        # Publish values to VESC
 
-            except KeyboardInterrupt:
-                self.drive_cmd.header.stamp = self.current_time
-                self.drive_cmd.header.frame_id = self.frame_id
-                self.drive_cmd.drive.speed = 0
-                self.drive_cmd.drive.steering_angle = 0
-                self.drive_pub.publish(self.drive_cmd)
-        else:
-            # publish drive control signal
-            self.drive_cmd.header.stamp = self.current_time
-            self.drive_cmd.header.frame_id = self.frame_id
-            self.drive_cmd.drive.speed = self.zero_speed
-            self.drive_cmd.drive.steering_angle = 0.0
-            self.drive_pub.publish(self.drive_cmd)
+        # if self.e_x < self.long_upper_error_threshold:
+        #     # Publish values
+        #     try:
+        #         # publish drive control signal
+        #         self.drive_cmd.header.stamp = self.current_time
+        #         self.drive_cmd.header.frame_id = self.frame_id
+        #         self.drive_cmd.drive.speed = speed
+        #         self.drive_cmd.drive.steering_angle = -delta
+        #         self.drive_pub.publish(self.drive_cmd)
+
+        #     except KeyboardInterrupt:
+        #         self.drive_cmd.header.stamp = self.current_time
+        #         self.drive_cmd.header.frame_id = self.frame_id
+        #         self.drive_cmd.drive.speed = 0
+        #         self.drive_cmd.drive.steering_angle = 0
+        #         self.drive_pub.publish(self.drive_cmd)
+        # else:
+        #     # publish drive control signal
+        #     self.drive_cmd.header.stamp = self.current_time
+        #     self.drive_cmd.header.frame_id = self.frame_id
+        #     self.drive_cmd.drive.speed = self.zero_speed
+        #     self.drive_cmd.drive.steering_angle = 0.0
+        #     self.drive_pub.publish(self.drive_cmd)
 
     def clamp(self, value, upper_bound, lower_bound=None):
         if lower_bound==None:
